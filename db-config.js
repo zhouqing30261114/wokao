5cc26ed6646fb394631bdfd23b05bf3c3e8c0b5e
zhouqing <i0324044>
2017-05-17 17:12:54 +0800

    delete all

--- db-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    host: 'localhost',
-    user: 'root',
-    password: '19840326',
-    database: 'zhouqing',
-    connectionLimit: 10,
-    supportBigNumbers: true
-};
--- modules/db.js
+++
@@ -1,3 +0,0 @@
-var mysql = require('mysql');
-var dbConfig = require('../db-config');
-module.exports = mysql.createPool(dbConfig);
